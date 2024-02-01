44baf51c5c38120ebc524a3df68958e4b743ac2e
Toradus <unesl@student.kit.edu>
2024-02-01 17:34:39 +0100

    added transform dict

--- scripts/test_loader.py
+++ scripts/test_loader.py
@@ -10,7 +10,7 @@ def main(cfg: DictConfig):
     # load Training Dataset from TensorflowDatasets
     dataset = get_octo_dataset_tensorflow(cfg, train=True)
     # create Pytorch Train Dataset
-    dataloader = make_pytorch_oxe_iterable_dataset(dataset, train=True, batch_size=512)
+    dataloader = make_pytorch_oxe_iterable_dataset(dataset, train=True, batch_size=512, transform_dict=cfg.transforms)
     for i, sample in tqdm.tqdm(enumerate(dataloader)):
         print("Top-level keys: ", sample.keys())
         print("Observation keys: ", sample["observation"].keys())
