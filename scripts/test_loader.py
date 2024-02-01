4e29527c21bd2ac1dc1daf2359bd445a3c9a92ed
Toradus <unesl@student.kit.edu>
2024-02-01 17:46:11 +0100

    forgot to_object

--- scripts/test_loader.py
+++ scripts/test_loader.py
@@ -2,15 +2,16 @@
 from uha import make_pytorch_oxe_iterable_dataset, get_octo_dataset_tensorflow
 import tqdm
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 
 @hydra.main(config_path="../uha/data/conf", config_name="uha_default_load_config")
 def main(cfg: DictConfig):
     # load Training Dataset from TensorflowDatasets
     dataset = get_octo_dataset_tensorflow(cfg, train=True)
+    cfg_transforms = OmegaConf.to_object(cfg.transforms)
     # create Pytorch Train Dataset
-    dataloader = make_pytorch_oxe_iterable_dataset(dataset, train=True, batch_size=512, transform_dict=cfg.transforms)
+    dataloader = make_pytorch_oxe_iterable_dataset(dataset, train=True, batch_size=512, transform_dict=cfg_transforms)
     for i, sample in tqdm.tqdm(enumerate(dataloader)):
         print("Top-level keys: ", sample.keys())
         print("Observation keys: ", sample["observation"].keys())
